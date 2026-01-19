34a12cfa7af83dfb59d705c00f28583d008193c8
Abd-Elrhman Mohamed Ghazy <abdelrhmanmohamedghazy2000@gmail.com>
2026-01-19 19:47:59 +0200

    Add files via upload

---
+++ StudentData.cs
@@ -0,0 +1,218 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace StudentApiDataAcessLayer
+{
+    public class StudentData
+    {
+        public class StudentDTO
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+            public int age { get; set; }
+            public int grade { get; set; }
+
+            public StudentDTO(int id, string name, int age, int grade)
+            {
+                this.id = id;
+                this.name = name;
+                this.age = age;
+                this.grade = grade;
+            }
+        }
+
+        static string _connectionString = "Server=localhost;Database=StudentsDB;User Id=sa;Password=sa123456;Encrypt=False;TrustServerCertificate=True;Connection Timeout=30;";
+
+        public static List<StudentDTO> GetAllStudents()
+        {
+            var StudentsList = new List<StudentDTO>();
+
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("SP_GetAllStudents", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    conn.Open();
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            StudentsList.Add(new StudentDTO
+                            (
+                                reader.GetInt32(reader.GetOrdinal("Id")),
+                                reader.GetString(reader.GetOrdinal("Name")),
+                                reader.GetInt32(reader.GetOrdinal("Age")),
+                                reader.GetInt32(reader.GetOrdinal("Grade"))
+                            ));
+                        }
+                    }
+                }
+
+
+                return StudentsList;
+            }
+        }
+
+
+        public static List<StudentDTO> GetPassedStudents()
+        {
+            var StudentsList = new List<StudentDTO>();
+
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("SP_GetPassedStudents", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    conn.Open();
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            StudentsList.Add(new StudentDTO
+                            (
+                                reader.GetInt32(reader.GetOrdinal("Id")),
+                                reader.GetString(reader.GetOrdinal("Name")),
+                                reader.GetInt32(reader.GetOrdinal("Age")),
+                                reader.GetInt32(reader.GetOrdinal("Grade"))
+                            ));
+                        }
+                    }
+                }
+
+
+                return StudentsList;
+            }
+        }
+
+
+        public static double GetAverageGrades()
+        {
+            double AVG = 0;
+
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("SP_GetAverageGrade", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    conn.Open();
+
+
+                    object result = cmd.ExecuteScalar();
+                    if (result != DBNull.Value)
+                    {
+                        AVG = Convert.ToDouble(result);
+                    }
+                    else
+                    {
+                        AVG = (double)0;
+                    }
+                    return AVG;
+
+                }
+            }
+        }
+
+
+
+        public static StudentDTO GetStudentById(int studentId)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            using (var command = new SqlCommand("SP_GetStudentById", connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@StudentId", studentId);
+
+                connection.Open();
+                using (var reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        return new StudentDTO
+                        (
+                            reader.GetInt32(reader.GetOrdinal("Id")),
+                            reader.GetString(reader.GetOrdinal("Name")),
+                            reader.GetInt32(reader.GetOrdinal("Age")),
+                            reader.GetInt32(reader.GetOrdinal("Grade"))
+                        );
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            }
+        }
+
+
+
+        public static int AddStudent(StudentDTO StudentDTO)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            using (var command = new SqlCommand("SP_AddStudent", connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@Name", StudentDTO.name);
+                command.Parameters.AddWithValue("@Age", StudentDTO.age);
+                command.Parameters.AddWithValue("@Grade", StudentDTO.grade);
+                var outputIdParam = new SqlParameter("@NewStudentId", SqlDbType.Int)
+                {
+                    Direction = ParameterDirection.Output
+                };
+                command.Parameters.Add(outputIdParam);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+
+                return (int)outputIdParam.Value;
+            }
+        }
+
+
+        public static bool UpdateStudent(StudentDTO StudentDTO)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            using (var command = new SqlCommand("SP_UpdateStudent", connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@StudentId", StudentDTO.id);
+                command.Parameters.AddWithValue("@Name", StudentDTO.name);
+                command.Parameters.AddWithValue("@Age", StudentDTO.age);
+                command.Parameters.AddWithValue("@Grade", StudentDTO.grade);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                return true;
+
+            }
+        }
+
+        public static bool DeleteStudent(int studentId)
+        {
+
+            using (var connection = new SqlConnection(_connectionString))
+            using (var command = new SqlCommand("SP_DeleteStudent", connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@StudentId", studentId);
+
+                connection.Open();
+
+                int rowsAffected = (int)command.ExecuteScalar();
+                return (rowsAffected == 1);
+
+
+            }
+        }
+
+
+
+    }
+}
